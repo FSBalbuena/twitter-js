61e02a3e1e62c876f12f04332a4e5bcf1cc0dfb0
FSBalbuena <fsbalbuena@gmail.com>
2018-01-24 10:46:46 -0300

    Comenzamos el resumen

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express=require('express')
 const app=express()//creamos una instancia de express
+/*const morgan=require('morgan')
+hacerlo luego con morgan*/
+
+app.use('/',function(req,res,next){
+	//podemos usar chalk
+	console.log(req)
+	next()
+})
+
+app.use('/special',function(req,res,next){
+	console.log('Legaste a special')
+	next()
+})
