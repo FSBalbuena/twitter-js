130953c2c2e85fd5cbc28b13a16bab347948e3c4
Amelie <dagostino.amelie@gmail.com>
2018-01-24 16:33:19 -0300

    agregado form

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
-const express=require('express')
-const router=express.Router()
+const express = require('express')
+const router = express.Router()
 const twBank = require('../tweetBank.js');
 
 router.get('/', function (req, res) {
   var tweets = twBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render('index', {
+    tweets: tweets,
+    showForm: true
+
+  });
 });
+
+router.get('/users/:name', function (req, res) {
+  var name = req.params.name;
+  var list = twBank.find({
+    name: name
+  });
+  res.render('index', {
+    tweets: list,
+    showForm: false
+  });
+});
+
+
 
 module.exports = router;
