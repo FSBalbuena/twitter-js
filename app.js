2692f84550dfbf2ad88b4b82f3b73a70ac4b2921
FSBalbuena <fsbalbuena@gmail.com>
2018-01-24 11:45:28 -0300

    usando git set

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express=require('express')
-const app=express()//creamos una instancia de express
+const app=express()
+const nj=require('nunjucks')//creamos una instancia de express
 /*const morgan=require('morgan')
 hacerlo luego con morgan*/
-
+app.engine('html', nj.render);
+app.set('view engine', 'html');
+nj.configure('views');
 app.use('/',function(req,res,next){
 	//podemos usar chalk
 	console.log(req)
@@ -13,3 +16,6 @@ app.use('/special',function(req,res,next){
 	console.log('Legaste a special')
 	next()
 })
+
+app.get('/',)
+
